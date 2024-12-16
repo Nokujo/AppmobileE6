1939dfce1fda004a00471866929333d849f9b6c5
Nokujo <mustaphachouhani2@gmail.com>
2024-12-16 18:51:31 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'OMDb API Demo',
+      home: MovieListScreen(),
+    );
+  }
+}
+
+class MovieListScreen extends StatefulWidget {
+  const MovieListScreen({super.key});
+
+  @override
+  _MovieListScreenState createState() => _MovieListScreenState();
+}
+
+class _MovieListScreenState extends State<MovieListScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  List<Movie> _movies = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('OMDb Movie Search'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(labelText: 'Search Movies'),
+              onSubmitted: (value) {
+                _searchMovies(value);
+              },
+            ),
+            const SizedBox(height: 16.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _movies.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_movies[index].title),
+                    subtitle: Text(_movies[index].year),
+                    leading: Image.network(_movies[index].poster),
+                    trailing: Text(_movies[index].type),
+                  
+
+                    
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _searchMovies(String query) async {
+    const apiKey = 'f41866d6';
+    final apiUrl = 'http://www.omdbapi.com/?apikey=$apiKey&s=$query';
+
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final List<dynamic> movies = data['Search'];
+
+      setState(() {
+        _movies = movies.map((movie) => Movie.fromJson(movie)).toList();
+      });
+    } else {
+      throw Exception('Failed to load movies');
+    }
+  }
+  
+  Texte(String type) {}
+}
+
+class Movie {
+  final String title;
+  final String year;
+  final String poster;
+  final String type;
+
+
+  Movie({required this.title, required this.year , required this.poster, required this.type, });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      title: json['Title'],
+      year: json['Year'],
+      poster: json['Poster'],
+      type: json['Type'],
+      
+
+    );
+  }
+}
